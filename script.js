186649e5c3d00082857fa29febd513f63a71434c
lll11233 <144907372+lll11233@users.noreply.github.com>
2025-04-23 12:57:10 +0930

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+function goHome(){
+    alert("Lets Go Home!")
+    setTimeout(() => {
+        alert("In 3 seconds ...");
+    }, 1000); // 3 seconds delay
+    // wait three seconds
+    setTimeout(() => {
+        window.location.href = "index.html";
+    }, 5000); // 3 seconds delay
+
+};
+
+
+function goReminder(){
+    alert("Lets learn about hydration!")
+    setTimeout(() => {
+        alert("In 3 seconds ...");
+    }, 1000); // 3 seconds delay
+    // wait three seconds
+    setTimeout(() => {
+        window.location.href = "reminder.html";
+    }, 5000); // 3 seconds delay
+
+};
+
+function goMedia(){
+    alert("Lets see water in all sorts of things!")
+    setTimeout(() => {
+        alert("In 3 seconds ...");
+    }, 1000); // 3 seconds delay
+    // wait three seconds
+    setTimeout(() => {
+        window.location.href = "media.html";
+    }, 5000); // 3 seconds delay
+
+};
+
+
+function goPinterest(){
+    alert("Lets see the pinterest inspiration!")
+    setTimeout(() => {
+        alert("In 3 seconds ...");
+    }, 1000); // 3 seconds delay
+    // wait three seconds
+    setTimeout(() => {
+        window.location.href = "404.html";
+        alert(404 + " page not found!");
+    }, 5000); // 3 seconds delay
+
+};
+
+
